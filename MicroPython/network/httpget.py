5c17d93902e53f15ca4d16f5394389256196b3a6
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2023-06-30 00:16:29 +0900

    to class

--- MicroPython/network/httpget.py
+++ MicroPython/network/httpget.py
@@ -1,49 +1,47 @@
 import network
 from time import sleep
 from picozero import pico_temp_sensor, pico_led
-import machine
 import urequests
-import ujson
+import env
 
 
 #
 # Wi-Fiに接続する関数です
 #
+class Network:
+    def __init__(self, ssid, password):
+        self.ssid = ssid
+        self.password = password
 
+    def Connect(self):
+        # Connect to WLAN
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(self.ssid, self.password)
+        while not wlan.isconnected():
+            print("Waiting for connection...")
+            sleep(1)
+        ip = wlan.ifconfig()[0]
+        return ip
 
-def connect(ssid, password):
-    # Connect to WLAN
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-    while not wlan.isconnected():
-        print("Waiting for connection...")
-        sleep(1)
-    ip = wlan.ifconfig()[0]
-    return ip
-
-
-def access(url):
-    print("start!!!")
-    try:
-        json_data = urequests.get(url)
-        print("dating!!!")
-        print(type(json_data))
-        print(json_data.text)
-
-    except OSError:
-        return False
-    return True
+    def Access(self, url):
+        print("start!!!")
+        try:
+            return True, urequests.get(url)
+        except OSError:
+            return False, ""
 
 
 if __name__ == "__main__":
-    # ssid = ""
-    # password = ""
-    ip = connect(ssid, password)
+    nw = Network(env.Ssid, env.Password)
+    ip = nw.Connect()
     print(f"connect{ip}")
     url = "http://zip.cgis.biz/xml/zip.php?zn=1030000"
 
     for num in range(10):
-        if access(url):
+        rst = nw.Access(url)
+        if rst[0]:
+            print(rst[1].text)
             break
+
         sleep(0.5)
